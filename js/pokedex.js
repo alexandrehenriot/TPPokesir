e2ed800233931d1c539a21d227cba672c2ad7d17
Bidibidibouh <31770318+Bidibidibouh@users.noreply.github.com>
2018-03-20 11:03:42 +0100

    Initial commit

---
+++ js/pokedex.js
@@ -0,0 +1,23 @@
+var pokeApp = angular.module('pokedex', ['ngResource']);
+
+// With this you can inject POKEAPI url wherever you want
+pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+
+pokeApp.config(['$resourceProvider', function($resourceProvider) {
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
+angular.module('getterSetterExample', [])
+    .controller('ExampleController', ['$scope',
+        function($scope) {
+    var _name = Brian;
+    $scope.user = {
+        name: function(newName) {
+            // Note that newName can be undefined for two reasons:
+            // 1. Because it is called as a getter and thus called with no arguments
+            // 2. Because the property should actually be set to undefined. This happens e.g. if the
+            //    input is invalid
+            return arguments.length ? (_name = newName) : _name;
+        }
+    };
+}]);
